667e01d97bef3a04f7ee072d2160c91f612474b8
Ilya <ilel@list.ru>
2026-03-11 19:43:19 +0700

    migrations add AddGalleryImages

---
+++ backend/Store.Api/Migrations/20260311090212_AddGalleryImages.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Store.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddGalleryImages : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "gallery_images",
+                columns: table => new
+                {
+                    id = table.Column<string>(type: "text", nullable: false),
+                    name = table.Column<string>(type: "text", nullable: false),
+                    description = table.Column<string>(type: "text", nullable: true),
+                    content_type = table.Column<string>(type: "text", nullable: false),
+                    file_extension = table.Column<string>(type: "text", nullable: false),
+                    file_name = table.Column<string>(type: "text", nullable: false),
+                    disk_path = table.Column<string>(type: "text", nullable: false),
+                    file_size = table.Column<long>(type: "bigint", nullable: false),
+                    binary_data = table.Column<byte[]>(type: "bytea", nullable: false),
+                    created_at = table.Column<long>(type: "bigint", nullable: false),
+                    updated_at = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_gallery_images", x => x.id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_gallery_images_name",
+                table: "gallery_images",
+                column: "name");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "gallery_images");
+        }
+    }
+}
